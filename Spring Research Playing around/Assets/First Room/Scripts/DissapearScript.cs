c9d65e65ccac1709428fd0652125d2221e2387e6
jgreenslade <jgreenslade@elon.edu>
2016-04-21 15:46:24 -0400

    Started Tutorials

---
+++ Spring Research Playing around/Assets/First Room/Scripts/DissapearScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DissapearScript : MonoBehaviour {
+
+  public Text text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void OnTriggerEnter () {
+    text.CrossFadeAlpha(0.0f, 1.0f, false);
+  }
+
+}
